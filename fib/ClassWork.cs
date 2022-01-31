4e5930b6609f297016ca1c525db7a6f00663e001
chelezo <alex .chelezo@gmail.com>
2022-01-31 22:24:15 +0200

    restavration

---
+++ fib/ClassWork.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fib
+{
+    class ClassWork
+    {
+        public void FibCycl(int fibcycle)
+        {
+           int FibCyk =Convert.ToInt32(fibcycle);
+            int FindFebN = Convert.ToInt32(FibCyk);
+            int _fibcycle = 0;
+            int b = 0;
+            int f = 1;
+
+            for (int a = 0; a < FindFebN; a++)
+            {
+
+                _fibcycle = b + f;
+                f = b;
+                b = _fibcycle;
+
+
+            }
+            Console.WriteLine($"Число Фибоначи = {_fibcycle}");
+
+        }
+
+
+
+
+    }
+
+     
+}
---
+++ fib/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace fib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassWork WorkC = new ClassWork();
+           System.Console.WriteLine("Введите число Фибоначи");
+           string n = Console.ReadLine();
+           int b = Convert.ToInt32(n);
+           WorkC.FibCycl(b);
+          
+        }
+
+    }
+}
